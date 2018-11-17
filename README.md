8a703391acb882a6cec75274855ac1b67ef33a0e
Márcio <emaildomso@gmail.com>
2018-11-17 09:26:37 -0200

    feat: Setup do projeto

---
+++ README.md
@@ -0,0 +1,3 @@
+# Udacity Full-Stack Developer (Projeto 5) - Mapa do Bairro
+
+Este projeto consiste num aplicativo de página única apresentando um mapa do bairro, incluindo locais em destaque, dados de terceiros sobre esses locais e diversas formas de navegar pelo conteúdo.
---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import logo from '../logo.svg';
+import '../index.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Neighborhood Map</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
