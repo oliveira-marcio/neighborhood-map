fd0d6e207f0d0b491379551469feeb4f77f20c8c
Márcio <emaildomso@gmail.com>
2018-11-20 01:05:46 -0200

    feat: Melhorado hover e InfoWindow para os markers
    - InfoWindow só aparece quando o ícone é clicado
    - Nesse caso, o tamanho do ícone permanece grande
    - Ao clicar em outro marker, o marker anterior volta ao normal e o novo
    fica ativo.
    - O mapa é centralizado no novo marker clicado

--- src/components/App.js
+++ src/components/App.js
@@ -2,37 +2,62 @@ import React, { Component } from 'react';
 import GoogleMapReact from 'google-map-react';
 import {Container, Segment, Header, Icon, Sidebar, Menu, Input} from 'semantic-ui-react';
 
-const Marker = ({text, $hover}) => {
-  return (
-    <div style={{
-      display: 'flex',
-      flexDirection: 'column',
-      alignItems: 'center',
-      justifyContent: 'flex-end',
-      position: 'absolute',
-      transform: 'translate(-50%, -100%)',
-      width: '70vh'
-    }}>
-      <Segment>
-        <Header as='h3' textAlign='center'>{text}</Header>
-        <Container text textAlign='justified'>
-          <p>
-            Texto Grande pra caramba que ocupe espaço à beça e quanto mais texto esperamos que quebre a linha corretamente.
-            Vamos botar mais texto para que o troço continue crescendo mais ainda.
-            Vamos botar mais texto para que o troço continue crescendo mais ainda.
-          </p>
-          <p>
-            Vamos botar mais texto para que o troço continue crescendo mais ainda.
-          </p>
-        </Container>
-      </Segment>
-      <Icon
-        name='map marker alternate'
-        size={$hover ? ("huge") : ("large")}
-        color={$hover ? ("teal") : ("red")}
-      />
-    </div>
-)};
+class Marker extends Component {
+  state = {
+    displayInfoWindow: false
+  };
+
+  toggleInfoWindow = () => {
+    this.setState({
+      displayInfoWindow: !this.state.displayInfoWindow
+    });
+  };
+
+  componentDidUpdate(prevProps){
+    const {selectedMarker, id} = this.props;
+    if((prevProps.selectedMarker !== selectedMarker) && (selectedMarker !== id)){
+      this.setState({ displayInfoWindow: false});
+    }
+  }
+
+  render(){
+    const {text, $hover} = this.props;
+    const {displayInfoWindow} = this.state;
+
+    return (
+      <div style={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'flex-end',
+        position: 'absolute',
+        transform: 'translate(-50%, -100%)',
+        width: '70vh'
+      }}>
+      {displayInfoWindow && (
+        <Segment>
+          <Header as='h3' textAlign='center'>{text}</Header>
+          <Container text textAlign='justified'>
+            <p>
+              Texto Grande pra caramba que ocupe espaço à beça e quanto mais texto esperamos que quebre a linha corretamente.
+              Vamos botar mais texto para que o troço continue crescendo mais ainda.
+              Vamos botar mais texto para que o troço continue crescendo mais ainda.
+            </p>
+            <p>
+              Vamos botar mais texto para que o troço continue crescendo mais ainda.
+            </p>
+          </Container>
+        </Segment>
+      )}
+        <Icon
+          name='map marker alternate'
+          size={displayInfoWindow || $hover ? ("huge") : ("large")}
+          color={displayInfoWindow|| $hover ? ("teal") : ("red")}
+          onClick={() => this.toggleInfoWindow()}
+        />
+      </div>
+    )};
+  }
 
 class App extends Component {
   state = {
@@ -41,15 +66,26 @@ class App extends Component {
       lng: -43.315725
     },
     zoom: 18,
-    sideBarVisible: false
+    sideBarVisible: false,
+    selectedMarker: -1
   };
 
   handleHideClick = () => this.setState({ sideBarVisible: false })
   handleShowClick = () => this.setState({ sideBarVisible: true })
   handleSidebarHide = () => this.setState({ sideBarVisible: false })
+  onChildClick = (key, childProps) => {
+    const {selectedMarker} = this.state;
+    this.setState({
+      selectedMarker: (selectedMarker === childProps.id ? -1 : childProps.id),
+      center: {
+        lat: childProps.lat,
+        lng: childProps.lng
+      }
+    });
+  }
 
   render() {
-    const { sideBarVisible } = this.state
+    const { sideBarVisible, selectedMarker } = this.state
 
     return (
       <Container fluid>
@@ -93,15 +129,24 @@ class App extends Component {
             <div style={{ margin: 0, height: '90vh', width: '100%' }}>
               <GoogleMapReact
                 bootstrapURLKeys={{ key: process.env.REACT_APP_MAPS_API_KEY }}
-                defaultCenter={this.state.center}
-                defaultZoom={this.state.zoom}
-                onChildMouseEnter={(i, x) => null}
-                onChildMouseLeave={(i, x) => null}
+                center={this.state.center}
+                zoom={this.state.zoom}
+                onChildClick={this.onChildClick}
               >
                 <Marker
+                  id={0}
                   lat={-22.849735}
                   lng={-43.315725}
                   text={'Pátio Carioca'}
+                  selectedMarker={selectedMarker}
+                />
+
+                <Marker
+                  id={1}
+                  lat={-22.850935}
+                  lng={-43.316325}
+                  text={'Outro'}
+                  selectedMarker={selectedMarker}
                 />
               </GoogleMapReact>
             </div>
