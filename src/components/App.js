d89eee609495d51a01ea0fb790e7bb6df6d9e214
Márcio Oliveira <emaildomso@gmail.com>
2018-11-21 16:00:52 -0200

    feat: Adicionada consulta por detalhes de um local

--- src/components/App.js
+++ src/components/App.js
@@ -65,7 +65,7 @@ class App extends Component {
       lat: -22.849735,
       lng: -43.315725
     },
-    zoom: 18,
+    zoom: 17,
     sideBarVisible: false,
     selectedMarker: -1
   };
@@ -88,7 +88,7 @@ class App extends Component {
       const FOURSQUARE_BASE_API_URL = 'https://api.foursquare.com/v2/venues/search?'
       const FOURSQUARE_CLIENT_ID = "client_id=" + process.env.REACT_APP_FOURSQUARE_CLIENT_ID
       const FOURSQUARE_CLIENT_SECRET = "&client_secret=" + process.env.REACT_APP_FOURSQUARE_CLIENT_SECRET
-      const API_VERSION = "&v=20130815"
+      const API_VERSION = "&v=20181121"
       const COORDINATES = "&ll=-22.849735,-43.315725"
       const QUERY = "&query=Patio Carioca"
       let url = FOURSQUARE_BASE_API_URL + FOURSQUARE_CLIENT_ID + FOURSQUARE_CLIENT_SECRET
@@ -97,14 +97,12 @@ class App extends Component {
       fetch(url).then(res => res.json()).then(({meta, response}) => {
         if(meta.code === 200){
           if(response.venues.length){
+            console.log(response.venues)
             const poi = response.venues[0]
             console.log(`Nome: ${poi.name}\n` +
-                        `Endereço: ${poi.location.address}\n` +
-                        `Cidade: ${poi.location.city}\n` +
-                        `Estado: ${poi.location.state}\n` +
-                        `Pais: ${poi.location.country}\n` +
-                        `CEP: ${poi.location.postalCode}\n` +
-                        `Categoria: ${poi.categories[0].name}`)
+                        `ID: ${poi.id}\n` +
+                        `Latitude: ${poi.location.lat}\n` +
+                        `Longitude: ${poi.location.lng}\n`)
           } else {
             console.log("Não encontrado")
           }
@@ -113,11 +111,39 @@ class App extends Component {
         }
       })
 
-      const id = "4bb667511344b713c6739d04" // Carioca Shopping
+      const id = "51e3d9f2498eaecbdfc906e8" // Pátio Carioca
+//      const id = "4bb667511344b713c6739d04" // Carioca Shopping
+      const FOURSQUARE_BASE_DETAILS_URL = `https://api.foursquare.com/v2/venues/${id}?`
+      url = FOURSQUARE_BASE_DETAILS_URL + FOURSQUARE_CLIENT_ID + FOURSQUARE_CLIENT_SECRET
+                + API_VERSION;
+
+      fetch(url).then(res => res.json()).then(({meta, response}) => {
+        if(meta.code === 200){
+          const {bestPhoto, canonicalUrl, categories, contact, hours, likes, location, name, rating, url} = response.venue
+          console.log(response.venue)
+          console.log(`Nome: ${name}\n` +
+                      `Endereço: ${location.address}\n` +
+                      `Cidade: ${location.city}\n` +
+                      `Estado: ${location.state}\n` +
+                      `Pais: ${location.country}\n` +
+                      `CEP: ${location.postalCode}\n` +
+                      `Foto: ${bestPhoto ? bestPhoto.prefix + '300x300' + bestPhoto.suffix : '-'}\n` +
+                      `URL (Foursquare): ${canonicalUrl}\n` +
+                      `URL (Local): ${url ? url : '-'}\n` +
+                      `Contato: ${contact ? contact.phone : '-'}\n` +
+                      `Horário: ${hours ? hours.status : '-'}\n` +
+                      `Likes: ${likes ? likes.count : '-'}\n` +
+                      `Avaliação: ${rating ? rating : '-'}\n` +
+                      `Categoria: ${categories.length ? categories[0].name : '-'}`)
+        } else {
+          console.log(`${meta.errorType}: ${meta.errorDetail}`)
+        }
+      })
+
+
       const FOURSQUARE_BASE_PHOTOS_URL = `https://api.foursquare.com/v2/venues/${id}/photos?`
-      const PHOTO_VERSION = "&v=20150603"
       url = FOURSQUARE_BASE_PHOTOS_URL + FOURSQUARE_CLIENT_ID + FOURSQUARE_CLIENT_SECRET
-                + PHOTO_VERSION;
+                + API_VERSION;
 
       fetch(url).then(res => res.json()).then(({meta, response}) => {
         if(meta.code === 200){
@@ -192,9 +218,9 @@ class App extends Component {
 
                 <Marker
                   id={1}
-                  lat={-22.850935}
-                  lng={-43.316325}
-                  text={'Outro'}
+                  lat={-22.849908}
+                  lng={-43.311197}
+                  text={'Carioca Shopping'}
                   selectedMarker={selectedMarker}
                 />
               </GoogleMapReact>
